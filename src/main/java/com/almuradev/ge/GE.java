3daae79b5819a179f5b6d2ef8057ed4f06a04278
Steven Downer <grinch@outlook.com>
2017-07-08 01:12:28 -0500

    Use appropriate Logger
    Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/com/almuradev/ge/GE.java
+++ src/main/java/com/almuradev/ge/GE.java
@@ -9,7 +9,7 @@ import com.almuradev.ge.util.Storage;
 import com.google.inject.Inject;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameConstructionEvent;
